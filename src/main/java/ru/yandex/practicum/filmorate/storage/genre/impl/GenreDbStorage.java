5fcf893f16fc9e59cc540bd4c7c9abb9bff68e09
NR <v.azarkin@gmail.com>
2023-04-10 01:13:28 +0300

    Some fix (style) (v10)

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/impl/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/impl/GenreDbStorage.java
@@ -18,6 +18,7 @@ import java.util.List;
 @Component
 public class GenreDbStorage implements GenreStorage {
     JdbcTemplate jdbcTemplate;
+
     @Override
     public Genre getById(int id) {
         try {
